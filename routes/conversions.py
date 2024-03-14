3b691a039245d651d9d0c24adaa144e46478f6b2
kevfo <75442525+kevfo@users.noreply.github.com>
2024-03-14 16:32:42 +0800

    Fixed improper usage of convert()

--- routes/conversions.py
+++ routes/conversions.py
@@ -3,11 +3,14 @@ from utils import numbers
 
 conversions = Blueprint('conversions', __name__)
 
-@conversions.route('/to_roman/<arabic>', methods = ['GET'])
+@conversions.route('/to_roman/<arabic>', methods = ['POST'])
 def to_roman(arabic):
-    print('this works!')
-    #jsonify({'result' : })
+    print(arabic)
+    result = numbers.convert(str(arabic), 'roman')
+    return(jsonify({'result' : result}))
 
-@conversions.route('/to_arabic/<roman>', methods = ['GET'])
+@conversions.route('/to_arabic/<roman>', methods = ['POST'])
 def to_arabic(roman):
-    print('this works')
+    print(roman)
+    result = numbers.convert(roman, 'arabic')
+    return(jsonify({'result' : result}))

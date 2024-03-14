56ff8a8fa43f4d969fd4e1405b0b56fe93ce9160
kevfo <75442525+kevfo@users.noreply.github.com>
2024-03-15 02:35:33 +0800

    Accounted for lowercase chars

--- utils/numbers.py
+++ utils/numbers.py
@@ -11,44 +11,49 @@ def convert(number: str, to: str) -> str:
     number system:
     '''
     try:
+        number = number.upper()
         if to == 'roman':
             result, number = '', int(number)
-            if number >= 1000:
-                result += 'M'
-                number -= 1000
-            elif number >= 900:
-                result += 'CM'
-                number -= 900
-            elif number >= 500:
-                result += 'D'
-                number -= 500
-            elif number >= 400:
-                result += 'CD'
-                number -= 400
-            elif number >= 100:
-                result += 'C'
-                number -= 100
-            elif number >= 90:
-                result += 'XC'
-                number -= 90
-            elif number >= 50:
-                result += 'L'
-                number -= 50
-            elif number >= 40:
-                result += 'XL'
-                number -= 40
-            elif number >= 9:
-                result += 'IX'
-                number -= 9
-            elif number >= 5:
-                result += 'V'
-                number -= 5
-            elif number >= 4:
-                result += 'IV'
-                number -= 4
-            else:
-                result += 'I'
-                number -= 1
+            while number > 0:
+                if number >= 1000:
+                    result += 'M'
+                    number -= 1000
+                elif number >= 900:
+                    result += 'CM'
+                    number -= 900
+                elif number >= 500:
+                    result += 'D'
+                    number -= 500
+                elif number >= 400:
+                    result += 'CD'
+                    number -= 400
+                elif number >= 100:
+                    result += 'C'
+                    number -= 100
+                elif number >= 90:
+                    result += 'XC'
+                    number -= 90
+                elif number >= 50:
+                    result += 'L'
+                    number -= 50
+                elif number >= 40:
+                    result += 'XL'
+                    number -= 40
+                elif number >= 10:
+                    result += 'X'
+                    number -= 10
+                elif number == 9:
+                    result += 'IX'
+                    number -= 9
+                elif number >= 5:
+                    result += 'V'
+                    number -= 5
+                elif number >= 4:
+                    result += 'IV'
+                    number -= 4
+                else:
+                    result += 'I'
+                    number -= 1
             return(result)
         elif to == 'arabic':
             result, index = 0, 0
@@ -60,11 +65,11 @@ def convert(number: str, to: str) -> str:
                         result += num1
                         index += 1
                     else:
-                        res += (num2 - num1)
-                        i += 2
+                        result += (num2 - num1)
+                        index += 2
                 else:
                     result += num1
                     index += 1
             return(str(result))
     except Exception as e:
-        print(f'The following error ocurred: "{e}"')
+        return('???')

ca57c3aeda122afe47d893a778e62b319582caf0
kevfo <75442525+kevfo@users.noreply.github.com>
2024-03-15 02:36:04 +0800

    Finished callbacks and error handling

--- static/js/convert.js
+++ static/js/convert.js
@@ -2,26 +2,27 @@
 to_arabic = () => {
     event.preventDefault();
     let user_input = document.querySelector('#arabic_input').value;
+    let result = document.querySelector('#arabic_results');
     fetch(`/to_roman/${user_input}`, {method : 'POST'})
     .then(data => data.json())
     .then(data => {
         let result = document.querySelector('#arabic_results');
         result.value = data.result;
     })
-    .catch(error => alert('Unable to convert; do check your input!'));
+    .catch(error => result.value = '???');
 }
 
 // Callback for converting numeric to roman characters.
 to_roman = () => {
     event.preventDefault();
     let user_input = document.querySelector('#roman_input').value;
+    let result = document.querySelector('#roman_result');
     fetch(`/to_arabic/${user_input}`, {method : 'POST'})
     .then(data => data.json())
     .then(data => {
-        let result = document.querySelector('#roman_result');
         result.value = data.result;
     })
-    .catch(error => alert('Unable to convert; do check your input!'));
+    .catch(error => result.value = '???');
 }
 
 // Attaching event listeners here:
